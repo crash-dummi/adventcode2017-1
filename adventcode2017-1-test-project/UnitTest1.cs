e59ced6456cef7df7f0e44c8c6095faed6fdf974
kevin-kline <21stcenturykline@gmail.com>
2018-11-21 19:42:15 -0400

    Add project files.

---
+++ adventcode2017-1-test-project/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace adventcode2017_1_test_project
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+    }
+}
---
+++ adventcode2017-1Tests/ProgramTests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using adventcode2017_1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adventcode2017_1.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        const string testInput1 = "1122";
+        const string testInput2 = "1111";
+        const string testInput3 = "1234";
+        const string testInput4 = "91212129";
+
+        const int testOutput1 = 3;
+        const int testOutput2 = 4;
+        const int testOutput3 = 0;
+        const int testOutput4 = 9;
+
+        [TestMethod]
+        public void getSumTest1()
+        {
+            int[] intArray1 = adventcode2017_1.Program.ConvertStringToIntArray(testInput1);
+            Assert.AreEqual(testOutput1, adventcode2017_1.Program.getSum(intArray1));
+        }
+
+        [TestMethod]
+        public void getSumTest2()
+        {
+            int[] intArray2 = adventcode2017_1.Program.ConvertStringToIntArray(testInput2);
+            Assert.AreEqual(testOutput2, adventcode2017_1.Program.getSum(intArray2));
+        }
+
+        [TestMethod]
+        public void getSumTest3()
+        {
+            int[] intArray3 = adventcode2017_1.Program.ConvertStringToIntArray(testInput3);
+            Assert.AreEqual(testOutput3, adventcode2017_1.Program.getSum(intArray3));
+        }
+        [TestMethod]
+        public void getSumTest4()
+        {
+            int[] intArray4 = adventcode2017_1.Program.ConvertStringToIntArray(testInput4);
+            Assert.AreEqual(testOutput4, adventcode2017_1.Program.getSum(intArray4));
+        }
+
+    }
+}
